2dcafc9b0426bb0020413d30e0e5df28c3955239
Rohith <Rohith@FFE-Rohith-FFE143>
2024-09-01 12:53:34 +0530

    first commit

---
+++ Framework/src/test/java/com/swagslab/testscript/Dummy.java
@@ -0,0 +1,15 @@
+package com.swagslab.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Dummy {
+	@Test
+	
+	public void openBrowser() {
+		Reporter.log("Successfully executed", true);
+		
+	}
+
+}
